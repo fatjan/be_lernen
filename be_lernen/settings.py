3df06f374980931a9fd9dab5ba1b0a645a9f84c6
Fatma <fatma.janna.utp@gmail.com>
2025-02-03 23:08:59 +0700

    update settings on debug and allowed hosts

--- be_lernen/settings.py
+++ be_lernen/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tt!qu&+9e1#asp#2mj-0+rpy7$h!6&m(@h#d*^v=)7ab3n(hci'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
